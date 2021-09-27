28efa7bc036dd532d35ac5519ee26ede002c9578
Marc <marc.brout@epitech.eu>
2021-09-27 16:34:28 +0200

    design: setup profile page design

--- app/src/main/java/com/example/takeofflabstinder/MainActivity.kt
+++ app/src/main/java/com/example/takeofflabstinder/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.takeofflabstinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.takeofflabstinder.databinding.ActivityMainBinding
+import com.example.takeofflabstinder.profiles.ProfilesFragment
+import com.example.takeofflabstinder.utils.with
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,7 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-
+        supportFragmentManager
+            .with(ProfilesFragment())
+            .into(binding.fullscreenFragmentHolder.id)
+            .push()
 
         setContentView(binding.root)
     }
